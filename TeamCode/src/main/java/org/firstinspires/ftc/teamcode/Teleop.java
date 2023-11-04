d523c09a5023922a437167a3f06c2b4f2576e311
sanjeevgedela <145948668+sanjeevgedela@users.noreply.github.com>
2023-11-04 14:48:51 -0500

    teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class Teleop extends LinearOpMode {
     DcMotor frontRight;
@@ -10,10 +11,10 @@ public class Teleop extends LinearOpMode {
     DcMotor backLeft;
     @Override
     public void runOpMode() throws InterruptedException {
-        frontRight=hardwareMap.get(DcMotor.class,"frontRight");
-        frontLeft=hardwareMap.get(DcMotor.class,"frontLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontRight=hardwareMap.get(DcMotorEx.class,"frontRight");
+        frontLeft=hardwareMap.get(DcMotorEx.class,"frontLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
 
 
         frontRight.setPower(gamepad1.left_stick_y-gamepad1.right_stick_x);
