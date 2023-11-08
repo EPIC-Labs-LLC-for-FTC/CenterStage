e0a76d64f9e3f13fe4075e85594e36897ae14b44
ayaz <111201806+Ayazislam6482@users.noreply.github.com>
2023-11-07 19:53:27 -0600

    robotTeleop

--- TeamCode/src/main/java/TeleOpTest1.java
+++ TeamCode/src/main/java/TeleOpTest1.java
@@ -3,6 +3,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOpTest1")
 public class TeleOpTest1 extends LinearOpMode {
@@ -12,11 +14,79 @@ public class TeleOpTest1 extends LinearOpMode {
     public DcMotorEx rightFront = null;
     public DcMotorEx leftBack = null;
     public DcMotorEx  rightBack = null;
+    public DcMotorEx arm = null;
+    public DcMotorEx spinTake = null;
+    public Servo door = null;
+    public Servo wrist = null;
+    public int targetPosition;
 
     double movement;
     double rotation;
     double strafe;
 
+    public void wristConrtol() {
+
+        boolean dpad_left = gamepad2.dpad_left;
+        boolean dpad_down = gamepad2.dpad_down;
+        double dropPosition = 1;
+        double pickupPosition = 0;
+
+        if (dpad_left) {
+            wrist.setPosition(dropPosition);
+        } else if (dpad_down) {
+            wrist.setPosition(pickupPosition);
+        }
+
+    }
+    public void doorContorl() {
+
+        boolean y = gamepad1.y;
+        boolean b = gamepad1.b;
+        double doorClose = 0;
+        double doorOpen = 1;
+
+        if (y) {
+            door.setPosition(doorClose);
+        } else if (b) {
+            door.setPosition(doorOpen);
+        }
+
+    }
+
+    public void spinnerControl() {
+
+        boolean right_bumper = gamepad2.right_bumper;
+        boolean left_bumper = gamepad2.left_bumper;
+
+        if (right_bumper) {
+            spinTake.setPower(-1);
+        } else if (left_bumper) {
+            spinTake.setPower(0);
+        }
+
+    }
+
+    public void armControl() {
+
+        double joystickInput = gamepad2.right_stick_y;
+        boolean right_bumper = gamepad1.right_bumper;
+
+        if (right_bumper) {
+            arm.setPower(-0.3);
+        }
+
+        if (joystickInput > 0.1 || joystickInput < -0.1) {
+            targetPosition += (int) (joystickInput * 100);
+            targetPosition = Range.clip(targetPosition, 0, arm.getCurrentPosition());
+        }
+            int currentPosition = arm.getCurrentPosition();
+            double power = (targetPosition - currentPosition) * 0.001;
+            arm.setPower(power);
+
+        telemetry.addData("Target Position", targetPosition);
+        telemetry.addData("Current Position", currentPosition);
+        telemetry.update();
+    }
     public void driverControl() {
         movement = gamepad1.left_stick_y;
         rotation = gamepad1.right_stick_x;
@@ -54,21 +124,36 @@ public class TeleOpTest1 extends LinearOpMode {
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
         leftBack = hardwareMap.get(DcMotorEx.class, "leftRear");
         rightBack = hardwareMap.get(DcMotorEx.class, "rightRear");
+        spinTake = hardwareMap.get(DcMotorEx.class, "spinTake");
+        door = hardwareMap.get(Servo.class,"door");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        arm = hardwareMap.get(DcMotorEx.class, "arm");
+
+        leftFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        arm.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
 
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        spinTake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        targetPosition = arm.getCurrentPosition();
 
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
                 driverControl();
+                armControl();
+                spinnerControl();
+                doorContorl();
+                wristConrtol();
             }
         }
     }
